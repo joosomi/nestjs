9c0761f3b447a783c3e0e1dcde6352dbc4e884f7
Somi Joo <109468380+joosomi@users.noreply.github.com>
2023-12-30 17:32:50 +0900

    Delete hi-nest directory

--- hi-nest/src/movies/dto/update-movie.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-import { fromEventPattern } from 'rxjs';
-import { CreateMovieDto } from './create-movie.dto';
-
-//optional -> ?
-
-//npm i @nestjs/mapped-types
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
--- hi-nest/src/movies/entities/movie.entity.ts
+++
@@ -1,8 +0,0 @@
-//실제로는 entities에 실제로 데이터베이스 모델을 만들어야 한다.
-
-export class Movie {
-  id: number;
-  title: string;
-  year: number;
-  genres: string[];
-}
